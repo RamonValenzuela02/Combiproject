3609f28290db954fe1f1ed6a51f12fea547a7f15
ra02valenzuela-eng <ra02valenzuela@gmail.com>
2026-05-26 08:55:51 -0300

    agrego microservicios de financial y wallet

---
+++ financial-service/src/test/java/com/RamonVale/financial_service/FinancialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RamonVale.financial_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ wallet-service/src/test/java/com/RamonVale/wallet_service/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RamonVale.wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
